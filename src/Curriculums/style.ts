b57c703eb8bd4e4f8c22fe97bae5384f6dcc7a27
Jisun Park <jisunpark.t@gmail.com>
2024-07-15 18:58:07 +0900

    Section1 ui -ing

--- src/Curriculums/style.ts
+++ src/Curriculums/style.ts
@@ -222,56 +222,56 @@ export const DetailSection = styled.section`
   width: 100%;
 `;
 
-export const Wrapper = styled.div`
+export const TitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
 
-export const Title = styled.h2`
-  font-family: Pretendard;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 34px;
-  text-align: center;
-  color: white !important;
-`;
+  & > h2 {
+    font-family: Pretendard;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 34px;
+    text-align: center;
+    color: white !important;
+  }
 
-export const Subtitle = styled.h3`
-  font-family: Pretendard;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 22px;
-  text-align: center;
-  color: rgb(255, 108, 122) !important;
-  margin-bottom: 8px;
-`;
+  & > h3 {
+    font-family: Pretendard;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    text-align: center;
+    color: rgb(255, 108, 122) !important;
+    margin-bottom: 8px;
+  }
 
-export const Description = styled.p`
-  font-family: Pretendard;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 22px;
-  text-align: center;
-  color: rgb(157, 167, 174) !important;
-`;
-
-export const Strong = styled.strong`
-  font-family: Pretendard;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 26px;
-  margin-top: 40px;
-  text-align: center;
-  color: rgb(242, 246, 248) !important;
-  display: inline-block;
+  & > p {
+    font-family: Pretendard;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    text-align: center;
+    color: rgb(157, 167, 174) !important;
+  }
 `;
 
 export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  & > strong {
+    font-family: Pretendard;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 26px;
+    margin-top: 40px;
+    text-align: center;
+    color: rgb(242, 246, 248) !important;
+    display: inline-block;
+  }
 `;
